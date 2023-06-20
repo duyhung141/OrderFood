8030d3aba81f2d0020c03695a6cfb05bdd08e845
duyhung141 <leduyhung141@gmail.com>
2023-06-20 22:42:10 +0700

    done layout.item_food

---
+++ app/src/main/java/com/example/orderfood/food/model/Food.java
@@ -0,0 +1,49 @@
+package com.example.orderfood.food.model;
+
+import android.net.Uri;
+
+public class Food {
+    private int id;
+    private String name;
+    private float price;
+    private Uri image;
+
+    public Food(int id, String name, float price, Uri image) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.image = image;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public Uri getImage() {
+        return image;
+    }
+
+    public void setImage(Uri image) {
+        this.image = image;
+    }
+}
